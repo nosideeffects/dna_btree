423b3adff0beecb59ccfb011cb808277ee294ec2
Jacob Biggs <biggs.jacob@gmail.com>
2012-12-10 22:25:47 -0700

    Added initial Sequence code

--- src/Sequence.java
+++ src/Sequence.java
@@ -1,3 +1,27 @@
+/**
+ * A Java Long disguised with a fancier name
+ * and some useful methods to represent a
+ * DNA sequence of length between 1 - 31.
+ * @author Jacob Biggs
+ *
+ */
 public class Sequence {
+	private Long seq;
+	
+	/**
+	 * Creates a new Sequence with the given
+	 * Long
+	 * @param seq
+	 */
+	public Sequence(Long seq){
+		
+	}
+	
 
+	public Sequence(String str){
+		if (str.length() == 0 || str.length() > 31){
+			throw new IllegalArgumentException("Sequence length was 0 or >31:\n" + str);
+		}
+	
+	}
 }
