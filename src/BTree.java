26a382d3c3e130530ea1fe5fbf7ebe3d4b7edd8b
Jacob Biggs <biggs.jacob@gmail.com>
2012-12-10 23:26:32 -0700

    Added basis for insert methods in BTree
    BTree.insert is as assignment describes
    BTreeNode.insert is equivalent to BTree.insertNonfull

--- src/BTree.java
+++ src/BTree.java
@@ -9,7 +9,6 @@ public class BTree<T extends Comparable<T>> {
 
 	public BTree(int degree, int sequenceLength) {
 		
-		//@TODO Do something
 		this.degree = degree;
 		this.sequenceLength = sequenceLength;
 		
@@ -24,11 +23,12 @@ public class BTree<T extends Comparable<T>> {
 			System.err.println("Invalid sequence length. Muse be an integer between 1 and 31 (inclusive).");
 			System.exit(4);
 		}
+		
+		this.root = new BTreeNode<T>();
 	}
 	
 	public BTree(String bTreeFile) {
 		
-		//@TODO Do something
 		try {
 			
 			FileInputStream fis = new FileInputStream(bTreeFile);
@@ -49,8 +49,16 @@ public class BTree<T extends Comparable<T>> {
 		}
 	}
 	
-	public void insert(T object){
-		
+	public void insert(T key){
+		BTreeNode<T> r = root;
+		if (r.isFull()) {
+			BTreeNode<T> s = new BTreeNode<T>();
+			this.root = s;
+			
+			s.setChild(0, r);
+		} else {
+			r.insert(key);
+		}
 	}
 	
 	private class BTreeNode<T> {
@@ -63,9 +71,44 @@ public class BTree<T extends Comparable<T>> {
 			this.leaf = true;
 			
 			this.keys = new ArrayList<BTreeNodeObject<T>>();
+			this.keys.ensureCapacity(degree * 2 - 1);
 			this.children = new ArrayList<BTreeNode<T>>();
+			this.children.ensureCapacity(degree*2);
 			
 			this.key = 0;
+		}
+		
+		public void setChild(int index, BTreeNode<T> node){
+			children.set(index, node);
+		}
+		
+		public BTreeNode<T> getChild(int index){
+			return children.get(index);
+		}
+		
+		public void splitChild(int index){
+			BTreeNode<T> z = new BTreeNode<T>();
+			BTreeNode<T> y = getChild(index);
+			
+			z.isLeaf(y.isLeaf());
+			
+			// TODO: split childrne into each node
+		}
+		
+		public void setKey(int index, BTreeNodeObject<T> obj){
+			keys.set(index, obj);
+		}
+		
+		public BTreeNodeObject<T> getKey(int index){
+			return keys.get(index);
+		}
+		
+		/**
+		 * Inserted key assuming node is not full.
+		 * @param key key to insert
+		 */
+		public void insert(T key){
+			
 		}
 		
 		/**
@@ -82,6 +125,14 @@ public class BTree<T extends Comparable<T>> {
 		 */
 		public boolean isFull(){
 			return n() == (2 * degree) - 1;
+		}
+		
+		public boolean isLeaf(){
+			return this.leaf;
+		}
+		
+		public void isLeaf(boolean leaf){
+			this.leaf = leaf;
 		}
 	}
 	
